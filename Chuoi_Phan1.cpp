7dac968c43bf5dbae792593378078165dbc2425b
your name <colammoicoan10@gmail.com>
2021-09-12 20:40:23 +0700

    Thêm hàm đếm số lượng từ có trong chuổi

--- Chuoi_Phan1.cpp
+++ Chuoi_Phan1.cpp
@@ -2,7 +2,7 @@
 #include<string.h>
 
 int DemCh(char s[]);
-
+int demSoTu(char s[]);
 int main()
 {
 	char s[100];
@@ -10,9 +10,26 @@ int main()
 	fflush(stdin);
 	gets(s);
 	printf("\nSo luong chu ch co trong chuoi la: %d",DemCh(s));
+	printf("\nSo tu co trong chuoi la: %d",demSoTu(s));
 	return 0;
 }
-
+int demSoTu(char s[])
+{
+	if(strlen(s) <= 1 && s[0] == ' ')
+		return 0;
+	else if(strlen(s) <= 1)
+		return 1;
+	
+	int count;
+	if(s[0] != ' ')
+		count = 1;
+	else
+		count = 0;
+	for(int i=1;i<strlen(s);i++)
+		if(s[i] != ' ' && s[i-1] == ' ')
+			count++;
+	return count;
+}
 int DemCh(char s[])
 {
 	int dem = 0;
